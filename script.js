21dbe618b3f6460264d1e27fcaa6598d518024f7
Rutwik187 <rutwikshinde34@gmail.com>
2023-02-18 23:41:36 +0530

    added changes

--- script.js
+++ script.js
@@ -108,12 +108,22 @@ let displayTestimonial = () => {
 };
 window.onload = displayTestimonial;
 
+// carousel
+const CarouselButtons = document.querySelectorAll("[data-carousel-button]")
 
-// gallery
-// document.querySelectorAll('.gallery-container video').forEach(vid => {
-//     vid.onclick = () => {
-//         console.log(vid);
-//         document.querySelector('.popup-video').style.display = 'block';
-//         document.querySelector('.popup-video video').src = vid.getAttribute('src');
-//     }
-// });
+CarouselButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button
+            .closest("[data-carousel]")
+            .querySelector("[data-slides]")
+
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
+
+        slides.children[newIndex].dataset.active = true
+        delete activeSlide.dataset.active
+    })
+})
